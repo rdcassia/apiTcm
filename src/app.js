498332618038ae7dd553f8b758eb187ffdbd3f26
Cassia <cassia@ssinformatica.net>
2020-03-13 15:53:51 -0300

    Ajuste Cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import path from 'path';
 import routes from './routes';
 
@@ -11,6 +13,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
